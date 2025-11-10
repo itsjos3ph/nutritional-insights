33a8701458c5a392bdf2ac8bd07ee4599a8fc7b8
Bdopz <bdopz0883@gmail.com>
2025-11-09 20:29:16 -0700

    Fix hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Image from "next/image";
-import { useApi } from "../hooks/useApi";
 import { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 
